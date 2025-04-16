009acc9deb54b49181eceeae9fe702a904984419
marcuslannister <marcus@lannister.cc>
2025-04-16 23:31:31 +0800

    feat: add more doom theme config and change theme to doom-one

--- post-init.el
+++ post-init.el
@@ -429,7 +429,19 @@
 (use-package doom-themes
   :ensure t
   :config
-  (load-theme 'doom-monokai-pro t))
+  (setq doom-themes-enable-bold t)
+  ;; Enable flashing mode-line on errors
+  (doom-themes-visual-bell-config)
+  ;; Enable custom neotree theme (nerd-icons must be installed!)
+  (doom-themes-neotree-config)
+  ;; or for treemacs users
+  (setq doom-themes-treemacs-theme "doom-atom") ; use "doom-colors" for less minimal icon theme
+  (doom-themes-treemacs-config)
+  ;; Corrects (and improves) org-mode's native fontification.
+  (doom-themes-org-config)
+
+  ;; (load-theme 'doom-monokai-pro t))
+  (load-theme 'doom-one t))
 
 ;; restart emacs command
 (use-package restart-emacs
