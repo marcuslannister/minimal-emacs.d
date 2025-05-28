43d8b291104117eaa199b19d3ce8b9da7cefbd68
marcuslannister <marcus@lannister.cc>
2025-05-28 22:30:18 +0800

    feat: add cape-dict

--- post-init.el
+++ post-init.el
@@ -79,6 +79,7 @@
   :init
   ;; Add to the global default value of `completion-at-point-functions' which is
   ;; used by `completion-at-point'.
+  (add-hook 'completion-at-point-functions #'cape-dict)
   (add-hook 'completion-at-point-functions #'cape-dabbrev)
   (add-hook 'completion-at-point-functions #'cape-file)
   (add-hook 'completion-at-point-functions #'cape-elisp-block))
